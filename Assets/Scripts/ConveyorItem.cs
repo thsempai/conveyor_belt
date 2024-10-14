e6c097c01e32898d568b8fb4eb52ed7d971edc7f
Thomas Stassin <thomas.stassin@interface3.be>
2024-10-14 15:51:35 +0200

    update + add scene

--- Assets/Scripts/ConveyorItem.cs
+++ Assets/Scripts/ConveyorItem.cs
@@ -1,8 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
-using Unity.VisualScripting;
 using UnityEngine;
-using UnityEngine.PlayerLoop;
+
 
 public class ConveyorItem : MonoBehaviour
 {
