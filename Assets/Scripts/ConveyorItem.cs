853fc2984117f4f3136a4fceafdfe7c14dc080d7
Thomas Stassin <thomas.stassin@interface3.be>
2024-10-14 11:36:58 +0200

    conveyor belt: ok

---
+++ Assets/Scripts/ConveyorItem.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.PlayerLoop;
+
+public class ConveyorItem : MonoBehaviour
+{
+    private Vector3 start;
+    private Vector3 end;
+
+    private float time;
+    private float chrono = 0f;
+    private ConveyorManager manager;
+
+    
+
+    public void Initialize(ConveyorManager manager, Vector3 start, Vector3 end, float speed){
+        this.manager = manager;
+        this.start = start;
+        this.end = end;
+        this.time = Vector3.Distance(start, end) / speed;
+        chrono = 0f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        chrono += Time.deltaTime;
+        transform.position = Vector3.Lerp(start, end, chrono/time);
+        if(chrono >= time){
+            manager.ItemArrived(this);
+        } 
+    }
+
+    void OnMouseDown(){
+        Destroy(gameObject);
+    }
+}
---
+++ Assets/Scripts/ConveyorManager.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class ConveyorManager : MonoBehaviour
+{
+    [SerializeField] float spawnRate = 1f;
+    float chrono = 0f;
+    [SerializeField] int batch = 50;
+    [SerializeField] GameObject prefab;
+    private Queue<ConveyorItem> queue = new();
+
+    [SerializeField]private Transform start;
+    [SerializeField]private Transform end;
+
+    [SerializeField]private float speed = 1f;
+
+    private void AddBatch(){
+        for(int _=0; _ < batch;_++){
+            GameObject newItem = Instantiate(prefab);
+            newItem.SetActive(false);
+            queue.Enqueue(newItem.GetComponent<ConveyorItem>());
+        }
+    }
+
+    private void Spawn(){
+        if(queue.Count == 0){
+            AddBatch();
+        }
+
+        ConveyorItem item = queue.Dequeue();
+        item.Initialize(this, start.position, end.position, speed);
+        item.gameObject.SetActive(true);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        chrono += Time.deltaTime;
+        if(chrono >= spawnRate){
+            chrono = 0f;
+            Spawn();
+        }
+    }
+
+    public void ItemArrived(ConveyorItem item){
+        item.gameObject.SetActive(false);
+        queue.Enqueue(item);
+    }
+}
